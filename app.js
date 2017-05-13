0ef222653b478b86635614e1f96271d713cde924
AgathaLynn <amaskelton@verizon.net>
2017-05-13 11:32:44 -0400

    removed unnecessary 'require' from app.js

--- app.js
+++ app.js
@@ -3,23 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-// for formatting reponses
-var data = require('./models/access.js');
-var format = require('./scripts/message/responses.js');
-
 // for getting data from database
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/fccbot');
-var Challenge = require('./models/challengeSchema.js');
 
 // for getting environmental variables
 require('dotenv').config();
 var app = express();
 
+// middleware and routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/', require('./controllers'));
 
 // start listening
