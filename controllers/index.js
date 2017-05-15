c58915e41c8ee45c6a0eca06468d7f3361c3147a
AgathaLynn <amaskelton@verizon.net>
2017-05-15 15:57:02 -0400

    added favicon and icon

--- controllers/index.js
+++ controllers/index.js
@@ -5,5 +5,6 @@ router.use('/', require('./home'));
 router.use('/oauth', require('./oauth'));
 router.use('/fccbot', require('./fccbot'));
 router.use('/actions', require('./actions'));
+router.use('/favicon.ico', express.static('../assets/favicon.ico'))
 
 module.exports = router;
